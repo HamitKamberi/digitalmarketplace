526df4a87ff921891ec5d0303aece6b707b2d928
Hamit Kamberi <kamberihamit4@gmail.com>
2025-07-02 16:03:14 +0200

    change sender email

---
+++ config/initializers/mailjet.rb
@@ -0,0 +1,5 @@
+Mailjet.configure do |config|
+  config.api_key = "723c14f72a847e1cf6af3ec57902f1a5"
+  config.secret_key = "beb48069e760a0b993355d4c5af9446b"
+  config.default_from = 'challengeeu.digitalmarketplace@gmail.com'
+end
