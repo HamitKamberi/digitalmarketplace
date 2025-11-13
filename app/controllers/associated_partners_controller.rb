1de890750441575d0775296f7f4879e80fe82ea9
Hamit Kamberi <kamberihamit4@gmail.com>
2025-11-13 16:02:46 +0100

    Avatar changes

--- app/controllers/associated_partners_controller.rb
+++ app/controllers/associated_partners_controller.rb
@@ -2,6 +2,6 @@ class AssociatedPartnersController < ApplicationController
   skip_authorization_check only: [:index]
 
     def index
-      @users = User.includes(:organization).where("users.role = ? OR organizations.id IS NOT NULL", :stakeholder).left_joins(:organization).distinct
+      @users = User.all
     end
 end
