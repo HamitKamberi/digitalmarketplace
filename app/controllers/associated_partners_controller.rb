ec3aa1b34eb45fcc2d24da942377e87b6a7afa4c
Hamit Kamberi <kamberihamit4@gmail.com>
2026-02-02 13:44:17 +0100

    Improve Members page, add filters
    Signed-off-by: Hamit Kamberi <kamberihamit4@gmail.com>

--- app/controllers/associated_partners_controller.rb
+++ app/controllers/associated_partners_controller.rb
@@ -1,7 +1,23 @@
 class AssociatedPartnersController < ApplicationController
   skip_authorization_check only: [:index]
 
-    def index
-      @users = User.all
-    end
+  def index
+    @roles = User.roles.keys
+    @countries = ["Germany", "North Macedonia", "France", "Sweden", "Portugal", "Poland", "Latvia", "Spain", "Switzerland"]
+    
+    @selected_roles = params[:roles].present? ? params[:roles].split(",") : []
+    @selected_countries = params[:countries].present? ? params[:countries].split(",") : []
+    
+    @users = filter_users
+    @users = @users.page(params[:page]).per(10)
+  end
+
+  private
+
+  def filter_users
+    users = User.all
+    users = users.where(role: @selected_roles) if @selected_roles.any?
+    users = users.where(country: @selected_countries) if @selected_countries.any?
+    users
+  end
 end
